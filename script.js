0efb27bc9ecdae030376c7e37fad479ed914ce49
adel <adelramdani27@gmail.com>
2024-10-11 16:02:55 +0100

    first

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const totalQ = document.getElementById("total-quantity");
 const totalP = document.getElementById("total-price");
 const cards = document.getElementsByClassName("product-card");
+const hearts = document.getElementsByClassName ('heart')
+
 
 for (let card of cards) {
   const productP = card.querySelector(".product-price");
   const add = card.querySelector(".add-quantity-button");
   const minus = card.querySelector(".minus-quantity-button");
   const quantity = card.querySelector(".quantity");
+  
 
   add.onclick = () => {
     quantity.innerText++;
@@ -20,5 +23,18 @@ for (let card of cards) {
       totalQ.innerText--;
       totalP.innerText = parseInt(totalP.innerText) - parseInt(productP.innerText);
     }
-  };
+  }
 }
+
+
+
+ for (let heart of hearts) {
+  heart.onclick = () => {
+    if (heart.style.color == 'red') {
+      heart.style.color = ''
+    }
+    else {
+      heart.style.color = 'red'
+    }
+  }
+ }
